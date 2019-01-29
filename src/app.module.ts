9d284ca3d640ce932ce6fe039affa609e4f16842
Benjamin Liden <benjamin.c.liden@gmail.com>
2019-01-29 14:15:04 -0500

    added error filter and logging interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipeModule } from './recipe/recipe.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 
 
 @Module({
   imports: [TypeOrmModule.forRoot(), RecipeModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor
+    }
+  ],
 })
 export class AppModule {}
--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, Logger } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { RecipeDTO } from './recipe.dto';
 
